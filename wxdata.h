7efd558ac9f8f2618df26b04957fddf616302e28
Tim Lampman <timothy.lampman@gmail.com>
2016-04-29 15:57:03 -0400

    Updates to data classes.

--- wxdata.h
+++ wxdata.h
@@ -3,8 +3,44 @@
 #ifndef __WXDATA_H__
 #define __WXDATA_H__
 
-// wxdata abstract class
-class wxdata {
+//=============================================================================
+// CloudLayer class
+//=============================================================================
+class CloudLayer {
+public:
+	// Public enum for layer type
+	enum Type { UNKN, FEW, SCT, BRK, OVC };
+	
+	// private members
+private:
+	Type _coverage;	// The type of cloud layer, in terms of sky coverage
+	int _base_agl;	// The height of the layer base, in feet AGL
+	
+public:
+	CloudLayer() { }	// Default 'do nothing' constructor
+	CloudLayer(Type layerType, int baseHeight);	// Initialize values
+	// Can I create a constructor that takes only the portion of the xml data relevent to the layer?
+	
+	// Data access
+	int getLayerBaseHeight() { return _base_agl; }
+	string getString();
+	
+	// Operators
+	bool operator <(const CloudLayer &ref_layer) { return this->_coverage < ref_layer._coverage; }
+	bool operator <=(const CloudLayer &ref_layer) { return this->_coverage <= ref_layer._coverage; }
+	bool operator >(const CloudLayer &ref_layer) { return this->_coverage > ref_layer._coverage; }
+	bool operator >=(const CloudLayer &ref_layer) { return this->_coverage >= ref_layer._coverage; }
+	bool operator ==(const CloudLayer &ref_layer) { return this->-coverage == ref_layer._coverage; }
+};
+
+//=============================================================================
+// WXObservation class
+//=============================================================================
+
+//=============================================================================
+// WXWrapper abstract class
+//=============================================================================
+class WXWrapper {
 protected:
 	// Protected members
 	string _data;		// The raw text for the observation
@@ -12,34 +48,26 @@ protected:
 	string _timestamp;	// The date/time the data was issued or observed
 	
 public:
-	wxdata() { };			// Do nothing constructor
-	wxdata(string xmlString);	// Constructor to set the text and extract the information
+	WXData() { };			// Do nothing constructor
+	WXData(string xmlString);	// Constructor to set the text and extract the information
 	
 	virtual void readXML(string xmlData) = 0;
 };
 
-// Metar structure
-struct Metar {
-    enum CloudType { UNKN, FEW, SCT, BKN, OVC };
-
-	string station;
-    string time;
+//=============================================================================
+// Metar class
+//=============================================================================
+class Metar : public WXWrapper {
     double temp;
     double wind_dir;
     double wind_vel;
     double pressure;
-//    double cloud_alt;
-//    CloudType cloud_type;
     string clouds;
-
-    void Clear() {
-		station = "";
-        time = "";
-        temp = wind_dir = wind_vel = pressure = 0.0;
-//        cloud_alt = -1.0;
-//        cloud_type = Metar::UNKN;
-        clouds = "";
-    }
 };
 
+//=============================================================================
+// Taf class
+//=============================================================================
+class Taf : public WXWrapper {
+};
 #endif	// __WXDATA_H__
